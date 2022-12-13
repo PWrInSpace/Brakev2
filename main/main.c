8eda5cc9524936140faa54221fda759ce19f71c9
jsiuda1 <jakkub68@gmail.com>
2022-12-13 19:57:19 +0100

    to do

--- main/main.c
+++ main/main.c
@@ -1,25 +1,35 @@
 // Copyright 2022 PWrInSpace
 
 #include <stdio.h>
-
-#include "driver/adc.h"
-#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 #include "voltageMeasure.h"
+#include "console.h"
 
 #define TAG "MAIN"
 
+
+int test(int argc, char** argv) {
+    ESP_LOGI(TAG, "Hello test");
+    return 0;
+}
+
+esp_console_cmd_t console_commands[] = {
+    {"test", "test1234", NULL, test, NULL}
+};
+
+
 void app_main(void) {
-  int i = 0;
-  voltageMeasure vBat = {.channel = ADC1_CHANNEL_4,
-                         .adcRaw = 0,
-                         .voltage = 0,
-                         .voltageDivider = 3.319727891};
-  voltageMeasureInit(&vBat);
-  while (1) {
-    ESP_LOGI(TAG, "Battery voltage: %f\n", getVoltage(&vBat));
-    i++;
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-  }
+    int i = 0;
+    console_init();
+    console_register_commands(console_commands, sizeof(console_commands)/sizeof(console_commands[0]));
+
+
+
+    while (1) {
+        ESP_LOGI(TAG, "[%d] Hello world!\n", i);
+        i++;
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
 }
