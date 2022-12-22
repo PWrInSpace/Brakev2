69ea1b7d263c1e0629af202bfd2670628983bd98
kgliwinski <k.gliwinski@gmail.com>
2022-12-22 23:52:21 +0100

    Fixes after review

--- main/main.c
+++ main/main.c
@@ -15,44 +15,8 @@
 
 void app_main(void) {
   int i = 0;
-  // voltageMeasure vBat = {.channel = ADC1_CHANNEL_4,
-  //                        .adcRaw = 0,
-  //                        .voltage = 0,
-  //                        .voltageDivider = 3.319727891};
-  // voltageMeasureInit(&vBat);
-  uint8_t data[8];
-    // ESP_ERROR_CHECK(i2c_master_init());
-    LPS25H lps;
-    uint8_t lpsReg = LPS25H_REG_WHO_AM_I;
-
-    ESP_ERROR_CHECK(LPS25HInit(&lps, SDA, SCL, I2C_NUM_1,
-    LPS25H_I2C_ADDR_SA0_H, NULL)); vTaskDelay(300 / portTICK_PERIOD_MS);
-    while (1) {
-      LPS25HRegisterRead(&lps, lpsReg, data, 8);
-      ESP_LOGI(TAG, "Who am I?: %X\n", data[0]);
-      i++;
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-  // i2c_config_t master;
-  // i2c_port_t port = I2C_NUM_1;
-
-  // master.mode = I2C_MODE_MASTER;
-  // master.sda_io_num = 21;
-  // master.scl_io_num = 22;
-  // master.sda_pullup_en = GPIO_PULLUP_ENABLE;
-  // master.scl_pullup_en = GPIO_PULLUP_ENABLE;
-  // master.master.clk_speed = 400000;
-  // master.clk_flags = 0;
-
-  // i2c_param_config(port, &master);
-  // i2c_driver_install(port, master.mode, 0, 0, 0);
-  // LPS25H lps;
-  // uint8_t lpsReg = LPS25H_REG_WHO_AM_I;
-  // ESP_ERROR_CHECK(LPS25HInit(&lps, 0, 0, port, LPS25H_I2C_ADDR_SA0_H, &master));
-  // while (1) {
-  //   LPS25HRegisterRead(&lps, lpsReg, data, 8);
-  //   ESP_LOGI(TAG, "Who am I?: %X\n", data[0]);
-  //   i++;
-  //   vTaskDelay(500 / portTICK_PERIOD_MS);
-  // }
+  while(1){
+    ESP_LOGI("ITER %d", i);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
