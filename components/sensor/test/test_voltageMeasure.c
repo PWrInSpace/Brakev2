4f4ad3efb9c78b32b2f1c0ca88baf9aa1eb7f992
kgliwinski <259282@student.pwr.edu.pl>
2023-01-09 12:21:25 +0100

    Finished voltageMeasure with tests

--- components/sensor/test/test_voltageMeasure.c
+++ components/sensor/test/test_voltageMeasure.c
@@ -1,9 +1,29 @@
 // Copyright 2022 PWrInSpace
 
-#include "unity.h"
+#include "esp_log.h"
 #include "sensor.h"
+#include "unity.h"
 #include "voltageMeasure.h"
 
-TEST_CASE("ADC Test", "[ADC]") {
-    TEST_ASSERT_EQUAL(32, sensor_return_value());
+#define BATT_ADC_CHANNEL ADC_CHANNEL_4
+#define BATT_ADC_CAL 3.3f
+#define VMES_LOG_TAG "vMes_test"
+static VoltageMeasure vMes;
+
+TEST_CASE("VoltageMeasure init test", "[ADC]") {
+  TEST_ASSERT_EQUAL(adcOK,
+                    voltageMeasureInit(&vMes, BATT_ADC_CHANNEL, BATT_ADC_CAL));
+}
+
+TEST_CASE("VoltageMeasure read test raw (7.4V on BATT ADC)", "[ADC]") {
+  // expected around 2850 in raw, 2,31V in voltage
+  int vRaw = voltageMeasureReadRaw(&vMes);
+  ESP_LOGI(VMES_LOG_TAG, "%d", vRaw);
+  TEST_ASSERT_INT_WITHIN(30, 2800, vRaw);
+}
+
+TEST_CASE("VoltageMeasure read test voltage (7.4V on BATT ADC)", "[ADC]") {
+  float vTest = voltageMeasureReadVoltage(&vMes);
+  ESP_LOGI(VMES_LOG_TAG, "%f", vTest);
+  TEST_ASSERT_FLOAT_WITHIN(0.2f, 7.4f, vTest);
 }
