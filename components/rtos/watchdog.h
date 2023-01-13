81aef8b9608b99b587bfaa9f2d304267b88cf993
kgliwinski <259282@student.pwr.edu.pl>
2023-01-13 12:45:47 +0100

    More watchdog

---
+++ components/rtos/watchdog.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define WATCHDOG_MAX_TASKS 16
+
+#ifndef WATCHDOG_MAX_TASKS
+#warning \
+    "To define the max amount of tasks the watchdog should track, define the WATCHDOG_MAX_TASKS macro"
+#endif
+
+typedef void (*watchdog_callback_t)(TaskHandle_t);
+
+void watchdog_background_task(void* arg);
+void watchdog_init(size_t delay, size_t watchdog_stack_size,
+                   size_t watchdog_task_priority, watchdog_callback_t callback);
+void watchdog_disconnect();
+void watchdog_reconnect(size_t delay, size_t watchdog_task_stack_size,
+                        size_t watchdog_task_priority,
+                        watchdog_callback_t callback);
+int watchdog_check_all();
+bool watchdog_append(TaskHandle_t feeder);
+bool watchdog_pop(TaskHandle_t feeder);
+int watchdog_feed(TaskHandle_t feeder);
+int watchdog_check(TaskHandle_t feeder);
+int watchdog_starve(TaskHandle_t feeder);
--- main/main.c
+++ main/main.c
@@ -1,14 +1,19 @@
 // Copyright 2022 PWrInSpace
 
 #include "rtos_tasks.h"
+#include "watchdog.h"
 #include "esp_log.h"
 
 #define TAG "MAIN"
+
+static watchdog_callback_t watchdogHandle;
 
 void app_main(void) {
     ESP_LOGI(TAG, "CREATING INIT TASK");
     if (xTaskCreate(init_task, "rtos init", 8000, NULL, 0, NULL) != pdPASS) {
         ESP_LOGE(TAG, "UNABLE TO CREATE INIT TASK");
     }
+    ESP_LOGI(TAG, "CREATING WATCHDOG TASK");
+    watchdog_init(100, 8000, TASK_PRIORITY_HIGH, watchdogHandle);
     vTaskDelete(NULL);
 }
